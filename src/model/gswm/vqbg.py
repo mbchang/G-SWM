6579bd02429f5d0d73fa0849d37688a2a44d561f
Michael Chang <mbchang2017@gmail.com>
2020-12-20 11:29:46 -0800

    clean up

--- src/model/gswm/vqbg.py
+++ src/model/gswm/vqbg.py
@@ -160,87 +160,7 @@ class VQBgModule(nn.Module):
             kl_bg=kl_bg,  # (B, T)  # VQ
         )
         return things
-    
-    # def generate(self, seq, cond_steps, sample):
-    #     """
-    #     Generate new frames given a set of input frames
-    #     Args:
-    #         seq: (B, T, 3, H, W)
 
-    #     Returns:
-    #         things:
-    #             bg: (B, T, 3, H, W)
-    #             kl: (B, T)
-    #     """
-    #     # (B, T, 3, H, W)
-    #     B, T, C, H, W = seq.size()
-        
-    #     # Encode images. Only needed for the first few steps
-    #     # (B*T, C, H, W)
-    #     enc = self.enc(seq[:, :cond_steps].reshape(B * cond_steps, 3, H, W))
-    #     # (B*T, D)
-    #     enc = enc.flatten(start_dim=1)
-    #     # (B*T, D)
-    #     enc = self.enc_fc(enc)
-    #     # (B, T, D)
-    #     enc = enc.view(B, cond_steps, ARCH.IMG_ENC_DIM)
-        
-    #     h_post = self.h_init_post.expand(B, ARCH.RNN_CTX_HIDDEN_DIM)
-    #     c_post = self.c_init_post.expand(B, ARCH.RNN_CTX_HIDDEN_DIM)
-    #     h_prior = self.h_init_prior.expand(B, ARCH.RNN_CTX_HIDDEN_DIM)
-    #     c_prior = self.c_init_prior.expand(B, ARCH.RNN_CTX_HIDDEN_DIM)
-    #     # (B,)
-    #     z_ctx_list = []
-    #     for t in range(T):
-            
-    #         if t < cond_steps:
-    #             # Compute posterior
-    #             # (B, D)
-    #             post_input = torch.cat([h_post, enc[:, t]], dim=-1)  # VQ
-    #             # (B, D), (B, D)
-    #             params = self.post_net(post_input)  # VQ
-    #             # (B, D), (B, D)
-    #             loc, scale = torch.chunk(params, 2, dim=-1)  # VQ
-    #             scale = F.softplus(scale) + 1e-4  # VQ
-    #             # (B, D)
-    #             z_ctx_post = Normal(loc, scale)  # VQ
-    #             # (B*T, D)
-    #             z_ctx = z_ctx_post.sample()  # VQ
-                
-    #             # Temporal encode
-    #             h_post, c_post = self.rnn_post(z_ctx, (h_post, c_post))
-    #             h_prior, c_prior = self.rnn_prior(z_ctx, (h_prior, c_prior))
-    #         else:
-    #             # Compute prior
-    #             params = self.prior_net(h_prior)  # VQ
-    #             loc, scale = torch.chunk(params, 2, dim=-1)  # VQ
-    #             scale = F.softplus(scale) + 1e-4  # VQ
-    #             z_ctx_prior = Normal(loc, scale)  # VQ
-    #             z_ctx = z_ctx_prior.sample() if sample else loc  # VQ
-    #             h_prior, c_prior = self.rnn_prior(z_ctx, (h_prior, c_prior))
-            
-    #         # Accumulate things
-    #         z_ctx_list.append(z_ctx)
-        
-    #     # (B, T, D) -> (B*T, D)
-    #     z_ctx = torch.stack(z_ctx_list, dim=1)
-    #     z_ctx = z_ctx.view(B * T, ARCH.Z_CTX_DIM)
-    #     # Before that, let's render our background
-    #     # (B*T, 3, H, W)
-    #     bg = self.dec(
-    #         # z_ctx
-    #         self.dec_fc(z_ctx).
-    #             view(B * T, 128, self.embed_size, self.embed_size)
-    #     )
-    #     bg = bg.view(B, T, 3, H, W)
-    #     # Split into lists of length t
-    #     z_ctx = z_ctx.view(B, T, ARCH.Z_CTX_DIM)
-    #     things = dict(
-    #         bg=bg,  # (B, T, 3, H, W)
-    #         z_ctx=z_ctx  # (B, T, D)
-    #     )
-        
-    #     return things
 
     def generate(self, seq, cond_steps, sample):
         """
@@ -282,23 +202,10 @@ class VQBgModule(nn.Module):
                 params = self.post_net(post_input)  # VQ
 
                 ###### VQ
-                # # (B, D), (B, D)
-                # loc, scale = torch.chunk(params, 2, dim=-1)  # VQ
-                # scale = F.softplus(scale) + 1e-4  # VQ
-                # # (B, D)
-                # z_ctx_post = Normal(loc, scale)  # VQ
-                # # (B*T, D)
-                # z_ctx = z_ctx_post.sample()  # VQ
-                # *************
                 z_ctx_q_x_st, z_ctx_q_x = self.codebook.straight_through(params)  #(B, D, h, w) both
                 ###### VQ
                 
                 ###### VQ
-                # # Temporal encode
-                # h_post, c_post = self.rnn_post(z_ctx, (h_post, c_post))
-                # h_prior, c_prior = self.rnn_prior(z_ctx, (h_prior, c_prior))
-                # ************
-                # Temporal encode
                 h_post, c_post = self.rnn_post(z_ctx_q_x_st, (h_post, c_post))
                 h_prior, c_prior = self.rnn_prior(z_ctx_q_x_st, (h_prior, c_prior))
                 ###### VQ
@@ -306,20 +213,12 @@ class VQBgModule(nn.Module):
                 # Compute prior
                 params = self.prior_net(h_prior)  # VQ
                 ###### VQ
-                # loc, scale = torch.chunk(params, 2, dim=-1)  # VQ
-                # scale = F.softplus(scale) + 1e-4  # VQ
-                # z_ctx_prior = Normal(loc, scale)  # VQ
-                # z_ctx = z_ctx_prior.sample() if sample else loc  # VQ
-                # h_prior, c_prior = self.rnn_prior(z_ctx, (h_prior, c_prior))
-                # *************
                 z_ctx_q_x_st, z_ctx_q_x = self.codebook.straight_through(params)  #(B, D, h, w) both 
                 h_prior, c_prior = self.rnn_prior(z_ctx_q_x_st, (h_prior, c_prior))
                 ###### VQ
             
             # Accumulate things
             ###### VQ
-            # z_ctx_list.append(z_ctx)
-            # ******
             z_ctx_list.append(z_ctx_q_x_st)
             ###### VQ
         
